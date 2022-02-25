8275c837fcaebdb16e9b48be354e0e6a4236e0cd
ZJ <68308477+ZJTAN97@users.noreply.github.com>
2022-02-25 22:39:34 +0800

    added events

--- Events/index.js
+++
@@ -1,1 +0,0 @@
-const logEvents = require("")
--- EventsAndServer/logEvents.js
+++ EventsAndServer/logEvents.js
@@ -7,13 +7,16 @@ const path = require("path");
 
 const logEvents = async (message) => {
     const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
-    const logItem = `${dateTime}\t${uuid()}\t${message}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
     console.log(logItem);
 
     try {
+        if(!fs.existsSync(path.join(__dirname, "logs"))) {
+            await fsPromises.mkdir(path.join(__dirname, "logs"));
+        }
         await fsPromises.appendFile(path.join(__dirname, "logs", "eventLog.txt"), logItem);
     } catch(err) {
-
+        console.log(err);
     }
 
 }
